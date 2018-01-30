dfaddb7bf2969dfd5e3bde9880cc159157457bde
subedib0 <subedib0@gmail.com>
2018-01-30 16:57:25 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/songform")
+
+    public String loadFormPage(Model model) {
+        model.addAttribute("song", new Song());
+        return "songform";
+
+
+    }
+
+    @PostMapping("/songform")
+    public String loadFormPage(@ModelAttribute Song song, Model model){
+        model.addAttribute("song", song);
+        return "confirmsong";
+
+
+    }
+
+}
